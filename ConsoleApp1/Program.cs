5e42161c3a1a7f515743bb24239a5ebb73063ae7
DaikiIo <nirasyuu@gmail.com>
2024-11-22 15:02:10 +0900

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Assemblyからバージョン情報を取得
+            var version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+            Console.WriteLine($"アプリケーションのバージョン: {version}");
+        }
+    }
+
+}
